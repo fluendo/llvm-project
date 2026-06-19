51b7aa3570500b29f9a5b4d5196a9542f8a7e58f
Jorge Zapata <jorgeluis.zapata@gmail.com>
2026-06-19 14:14:59 +0200

    wasm: Improve the runtime casts
    Use a direct wrapper for immediate casts and a set of wrappers
    for saved casted function pointers. Include a cache mechamism
    and a documentation for the whole approach.

--- clang/lib/CodeGen/TargetInfo.h
+++ clang/lib/CodeGen/TargetInfo.h
@@ -439,7 +439,7 @@ public:
   /// that need signature adaptation.
   virtual llvm::Value *emitWasmRuntimeFunctionPointerBinding(
       CodeGenFunction &CGF, llvm::Value *FnPtr, QualType SrcType,
-      QualType DstType) const {
+      QualType DstType, bool IsImmediate) const {
     return nullptr;
   }
 
